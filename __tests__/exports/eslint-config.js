1e6dc3e99aff00f2157efcd375e9cc62fb272a51
Alexey Ryabov <lesha_12_01@mail.ru>
2023-11-22 12:36:11 +0300

    chore: clear all archived files

--- __tests__/exports/eslint-config.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-// $FlowFixMe
-const config = require('eslint-config-datarockets') // eslint-disable-line
-const isPlainObject = require('lodash/isPlainObject')
-
-it('is an object', () => {
-  expect(isPlainObject(config)).toBeTruthy()
-})
